3936cdeccd40f6b804f144e9dab1838e7c1b7eaf
lucas wolder <>
2021-05-10 09:26:47 +0200

    API scraping added

--- webApp/src/App.js
+++ webApp/src/App.js
@@ -4,14 +4,20 @@ import './App.css';
 import Header from './components/header';
 import Layout from './components/layout';
 import { render } from '@testing-library/react';
-import { getEmployees } from './utils/api';
+import { getEmployees, getInitialData, checkProgres } from './utils/api';
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       profiles: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
-      profiles2: []
+      profiles2: [],
+      apiKey: "",
+      interval: 0,
+      total: 0,
+      done: 0,
+      APIvisible: true,
+      downloadVisible: false,
     }
   }
 
@@ -20,14 +26,37 @@ class App extends React.Component {
   }
   getEmp = async () => {
     let data = await getEmployees();
-        this.setState({profiles2:data})
-        console.log(data)
+    console.log(data);
+    this.setState({ profiles2: data });
+  }
+  handleChange = (e) => {
+    this.setState({ apiKey: e.target.value });
+  }
+  handleSubmit = async () => {
+    console.log(this.state.apiKey)
+    let response = await getInitialData(this.state.apiKey)
+    response == 1 ? this.getEmp() : console.log(response)
+    this.setState({interval:setInterval(() => {
+      this.checkProgress()
+    }, 1000)})
+  }
+  checkProgress = async () => {
+    let response = await checkProgres()
+    console.log(response)
+    console.log(response.done)
+    this.setState({total:response.total,done:response.done})
+    if (response.done >= response.total) {
+      console.log(response.done > response.total)
+      clearInterval(this.state.interval)
     }
+  }
+
 
   render() {
   return (
     <div className="App">
       <Header></Header>
+      <div><label>Please provide API key:<input value={this.state.apiKey} onChange={(e) => { this.handleChange(e) }}></input></label><button onClick={() => this.handleSubmit()}>submit</button><label> Downloaded {this.state.done} out of {this.state.total} images</label></div>
       <Layout profiles={this.state.profiles2}></Layout>
     </div>
   );
--- webApp/src/utils/api.js
+++ webApp/src/utils/api.js
@@ -2,12 +2,41 @@ import axios from "axios";
 
 export async function getEmployees() {
   try {
-      const response = await axios.get("https://api.1337co.de/v3/employees", {
-          headers: {
-              Authorization: "api-key 14:2021-04-30:lucas.stenberg@tretton37.com 98bbcb9671b4b2bbd13062a20ce47fd6cbfb81ee3898bb5ac0b2f8c0d470d632"
-          }
+    //const response = await axios.get("https://api.1337co.de/v3/employees", {
+        const response = await axios.get("http://localhost:3001/api/getEmployees", {
+          // headers: {
+          //     //Authorization: "api-key 14:2021-04-30:lucas.stenberg@tretton37.com 98bbcb9671b4b2bbd13062a20ce47fd6cbfb81ee3898bb5ac0b2f8c0d470d632"
+          // }
       }
-      );
+        );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    return "err";
+  }
+}
+
+export async function getInitialData(apiKey) {
+  console.log(apiKey)
+  try {
+    const response = await axios.get("http://localhost:3001/api/getInintialData",
+      {
+         params: {
+        key:apiKey
+      }
+      },
+        );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    return "err";
+  }
+}
+
+export async function checkProgres() {
+  try {
+    const response = await axios.get("http://localhost:3001/api/checkProgress",
+        );
     return response.data;
   } catch (error) {
     console.log(error);
